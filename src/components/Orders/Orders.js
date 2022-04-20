4f1d820aa5d34919883897ee81ed221d4144d99a
Programming Hero <programming.hero1@gmail.com>
2022-04-19 21:36:25 -0500

    load data from server

--- src/components/Orders/Orders.js
+++ src/components/Orders/Orders.js
@@ -12,9 +12,9 @@ const Orders = () => {
     const [cart, setCart] = useCart(products);
     const navigate = useNavigate();
     const handleRemoveProduct = product =>{
-        const rest = cart.filter(pd => pd.id !== product.id);
+        const rest = cart.filter(pd => pd._id !== product._id);
         setCart(rest);
-        removeFromDb(product.id);
+        removeFromDb(product._id);
     }
 
     
@@ -23,7 +23,7 @@ const Orders = () => {
             <div className="review-items-container">
                 {
                     cart.map(product => <ReviewItem
-                        key={product.id}
+                        key={product._id}
                         product ={product}
                         handleRemoveProduct = {handleRemoveProduct}
                     ></ReviewItem>)
--- src/hooks/useCart.js
+++ src/hooks/useCart.js
@@ -8,7 +8,7 @@ const useCart = (products) => {
         const storedCart = getStoredCart();
         const savedCart = [];
         for(const id in storedCart){
-            const addedProduct = products.find(product => product.id === id);
+            const addedProduct = products.find(product => product._id === id);
             if(addedProduct){
                 const quantity = storedCart[id];
                 addedProduct.quantity = quantity;
